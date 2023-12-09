74be1199bee2fd6c77b37f7772e7f6e7af36bd6a
Radifire <Jaghoghov@gmail.com>
2023-12-09 18:44:08 +0000

    fixed merge conflict

--- app/api/read_table/route.js
+++ app/api/read_table/route.js
@@ -1,9 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getUserData } from '../../../lib/dbOperations'; // Adjust the path as necessary
-<<<<<<< HEAD
 
-=======
->>>>>>> 089edd23d5b4fe645237fe19164acf7433efb5ad
 export async function POST (req) {
   try {
     const body = await req.json();
@@ -12,10 +9,6 @@ export async function POST (req) {
     const data = await getUserData(username);
 
     return NextResponse.json({ data });
-<<<<<<< HEAD
-=======
-)
->>>>>>> 089edd23d5b4fe645237fe19164acf7433efb5ad
   } catch (error) {
     console.error('Error fetching data from the database', error);
     return NextResponse.error({ status: 500, message: 'Internal Server Error' });
