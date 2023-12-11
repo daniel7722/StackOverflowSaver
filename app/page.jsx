962694420301d09498f79c3800bd41eb7ed7d7fe
Daniel Huang <74921405+daniel7722@users.noreply.github.com>
2023-12-11 11:06:28 +0000

    Updata page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -9,8 +9,6 @@ export default function Home () {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Login/Signup</title>
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link> */}
       </Head>
       <div className='loginContainer'>
         <Login />
